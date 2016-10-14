a3c2967626119be486db1cc5cee70022a95a7366
Tolmachev.Kirill <teleonet@gmail.com>
2016-10-14 23:17:23 +0300

    refactoring MainActivity

--- app/src/main/java/science/ksp/buttonchecker/MainActivity.java
+++ app/src/main/java/science/ksp/buttonchecker/MainActivity.java
@@ -1,45 +1,50 @@
 package science.ksp.buttonchecker;
 
-import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button buttonSetColor;
-    Button buttonSetImage;
-    TextView colorField;
-    ImageView imageView;
+    private Button buttonSetColor;
+    private Button buttonSetImage;
+    private TextView colorField;
+    private ImageView imageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         buttonSetColor = (Button) findViewById(R.id.buttonSetColor);
         buttonSetImage = (Button) findViewById(R.id.buttonSetImage);
         colorField = (TextView) findViewById(R.id.colorField);
         imageView = (ImageView) findViewById(R.id.imageView);
-
-        buttonSetColor.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                colorField.setText(R.string.errorMessage);
-                colorField.setBackgroundColor(R.color.colorRed);
-                colorField.setTextColor(R.color.colorYellow);
-            }
-        });
-        buttonSetImage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                setContentView(imageView.VISIBLE);
-            }
-        });
+        setClickListeners();
     }
 
+    private void setClickListeners() {
+        buttonSetImage.setOnClickListener(this);
+        buttonSetColor.setOnClickListener(this);
+    }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.buttonSetColor:
+                colorField.setText(R.string.errorMessage);
+                colorField.setBackgroundColor(getResources().getColor(R.color.colorRed));
+                colorField.setTextColor(getResources().getColor(R.color.colorYellow));
+                break;
+            case R.id.buttonSetImage:
+                if (imageView.getVisibility() == View.VISIBLE) {
+                    imageView.setVisibility(View.INVISIBLE);
+                } else {
+                    imageView.setVisibility(View.VISIBLE);
+                }
+                break;
+        }
+    }
 }
